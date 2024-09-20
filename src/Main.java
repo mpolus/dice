14ac346711af0d6f861bae0ee9417532b6890584
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-09-20 13:30:42 -0400

    dice

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        rollSixDice();
+        rollSixDice();
+        rollSixDice();
+        rollSixDice();
+    }
+
+    private static void rollSixDice() {
+        Random random = new Random();
+        System.out.print("6 dices roll: ");
+        for (int i = 0; i < 6; i++){
+            int roll = random.nextInt(6) + 1;
+            System.out.print("Roll #" + (i + 1) + ": " + roll + " ");
+
+        }
+        System.out.println();
+    }
 }
